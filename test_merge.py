ed434ccebab773fa843cb06174be28eac8d8897b
MAXParfenyuk <37433656+MAXParfenyuk@users.noreply.github.com>
2023-11-08 19:00:02 +0100

    #1 + #2 + updated tests
    #1
    Renamed attributes _context to votes and _status to status to make their purposes clear.
    Updated method names status to get_status and vote to record_vote to reflect their actions more accurately.
    
    #2
    Constants are used to represent the different status values of the merge request (OPEN, CLOSED, APPROVED, REJECTED, PENDING), addressing the "Primitive Obsession" code smell.
    This change makes the code more robust and maintainable by avoiding the use of magic strings for status.
    
    tests:
    Replaced the usage of magic strings with constants for status (e.g., "open" is now MergeRequest.OPEN, "closed" is now MergeRequest.CLOSED, and so on).
    Updated test cases to use the updated method names and constants for status, making the tests more readable and consistent.

--- test_merge.py
+++ test_merge.py
@@ -1,83 +1,79 @@
 import unittest
-
 from merge import MergeRequest
-import unittest
-
 
 class TestMergeRequest(unittest.TestCase):
     def setUp(self):
         self.mr = MergeRequest()
 
     def test_close_approved(self):
-        self.mr.vote("Alice", "upvote")
-        self.mr.vote("Bob", "upvote")
+        self.mr.record_vote("Alice", "upvote")
+        self.mr.record_vote("Bob", "upvote")
         self.assertEqual(self.mr.close(), "Merge request has been approved and closed")
-        self.assertEqual(self.mr.status(), "closed")
+        self.assertEqual(self.mr.get_status(), MergeRequest.CLOSED)
 
     def test_close_rejected(self):
-        self.mr.vote("Alice", "downvote")
-        self.mr.vote("Bob", "downvote")
+        self.mr.record_vote("Alice", "downvote")
+        self.mr.record_vote("Bob", "downvote")
         self.assertEqual(self.mr.close(), "Merge request has been rejected and closed")
-        self.assertEqual(self.mr.status(), "closed")
+        self.assertEqual(self.mr.get_status(), MergeRequest.CLOSED)
 
     def test_close_pending(self):
         self.assertEqual(
             self.mr.close(),
             "Cannot close merge request until it has been approved or rejected",
         )
-        self.assertEqual(self.mr.status(), "pending")
+        self.assertEqual(self.mr.get_status(), MergeRequest.PENDING)
 
     def test_getvotes_no_votes(self):
-        self.assertEqual(self.mr.getvotes(), "No votes yet")
+        self.assertEqual(self.mr.get_vote_summary(), "No votes yet")
 
     def test_getvotes_only_upvotes(self):
-        self.mr.vote("Alice", "upvote")
-        self.assertEqual(self.mr.getvotes(), "1 upvotes")
+        self.mr.record_vote("Alice", "upvote")
+        self.assertEqual(self.mr.get_vote_summary(), "1 upvotes")
 
     def test_getvotes_only_downvotes(self):
-        self.mr.vote("Alice", "downvote")
-        self.assertEqual(self.mr.getvotes(), "1 downvotes")
+        self.mr.record_vote("Alice", "downvote")
+        self.assertEqual(self.mr.get_vote_summary(), "1 downvotes")
 
     def test_getvotes_upvotes_and_downvotes(self):
-        self.mr.vote("Alice", "upvote")
-        self.mr.vote("Bob", "downvote")
-        self.assertEqual(self.mr.getvotes(), "1 upvotes, 1 downvotes")
+        self.mr.record_vote("Alice", "upvote")
+        self.mr.record_vote("Bob", "downvote")
+        self.assertEqual(self.mr.get_vote_summary(), "1 upvotes, 1 downvotes")
 
     def test_vote_upvote(self):
-        self.assertEqual(self.mr.vote("Alice", "upvote"), None)
-        self.assertEqual(self.mr.status(), "pending")
-        self.assertEqual(self.mr.vote("Bob", "upvote"), None)
-        self.assertEqual(self.mr.status(), "approved")
+        self.assertEqual(self.mr.record_vote("Alice", "upvote"), None)
+        self.assertEqual(self.mr.get_status(), MergeRequest.PENDING)
+        self.assertEqual(self.mr.record_vote("Bob", "upvote"), None)
+        self.assertEqual(self.mr.get_status(), MergeRequest.APPROVED)
 
     def test_vote_downvote(self):
-        self.assertEqual(self.mr.vote("Alice", "downvote"), None)
-        self.assertEqual(self.mr.status(), "rejected")
+        self.assertEqual(self.mr.record_vote("Alice", "downvote"), None)
+        self.assertEqual(self.mr.get_status(), MergeRequest.REJECTED)
 
     def test_vote_closed(self):
-        self.mr.vote("Alice", "upvote")
-        self.mr.vote("Bob", "upvote")
+        self.mr.record_vote("Alice", "upvote")
+        self.mr.record_vote("Bob", "upvote")
         self.mr.close()
         self.assertEqual(
-            self.mr.vote("Alice", "upvote"), "can't vote on a closed merge request"
+            self.mr.record_vote("Alice", "upvote"), "Can't vote on a closed merge request"
         )
 
     def test_status_pending(self):
-        self.assertEqual(self.mr.status(), "pending")
-        self.mr.vote("Alice", "upvote")
-        self.assertEqual(self.mr.status(), "pending")
-        self.mr.vote("Bob", "downvote")
-        self.assertEqual(self.mr.status(), "rejected")
+        self.assertEqual(self.mr.get_status(), MergeRequest.PENDING)
+        self.mr.record_vote("Alice", "upvote")
+        self.assertEqual(self.mr.get_status(), MergeRequest.PENDING)
+        self.mr.record_vote("Bob", "downvote")
+        self.assertEqual(self.mr.get_status(), MergeRequest.REJECTED)
 
     def test_status_approved(self):
-        self.mr.vote("Alice", "upvote")
-        self.mr.vote("Bob", "upvote")
-        self.assertEqual(self.mr.status(), "approved")
+        self.mr.record_vote("Alice", "upvote")
+        self.mr.record_vote("Bob", "upvote")
+        self.assertEqual(self.mr.get_status(), MergeRequest.APPROVED)
 
     def test_status_rejected(self):
-        self.mr.vote("Alice", "downvote")
-        self.mr.vote("Bob", "downvote")
-        self.assertEqual(self.mr.status(), "rejected")
-
+        self.mr.record_vote("Alice", "downvote")
+        self.mr.record_vote("Bob", "downvote")
+        self.assertEqual(self.mr.get_status(), MergeRequest.REJECTED)
 
 if __name__ == "__main__":
     unittest.main()
